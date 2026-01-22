99d99041901abdaa2dc93b498d9551ff7e7df160
Sekou <sekouk1410@gmail.com>
2026-01-22 00:17:03 +0000

    feat: configuration Firebase et Angular Material avec thème personnalisé

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+// Configuration Firebase pour l'environnement de production
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "VOTRE_API_KEY",
+        authDomain: "VOTRE_AUTH_DOMAIN",
+        projectId: "VOTRE_PROJECT_ID",
+        storageBucket: "VOTRE_STORAGE_BUCKET",
+        messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
+        appId: "VOTRE_APP_ID"
+    }
+};
